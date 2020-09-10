5f0e038d2934e00bd4aaff0f993d5aedfe0d0144
sqrtpi177 <solar0037@gmail.com>
2020-09-10 23:22:12 +0900

    add anime ranking

--- main.py
+++ main.py
@@ -35,131 +35,190 @@ async def on_message(message):
     if content.startswith(bot_call):
         text = content.replace(bot_call, "").strip()  # text excluding "호돌아 "
 
-        # ##################################################
-        # commands for playing
+        # default=True -> check for abbreviations
+        default = True
+
+        ##################################################
+        #               commands for playing             #
+        ##################################################
         for s in ['안녕', '안뇽', '아령', '하이', 'ㅎㅇ']:
             if s in text:
                 await channel.send("어흥~!")
+                default = False
                 break
 
         for s in ['잘가' '잘 가', '꺼져', 'ㄲㅈ', 'ㅃ2', '빠이', '바이']:
             if s in text:
                 await channel.send("안 된다 이 악마야!")
+                default = False
                 break
 
         for s in ['말해봐', '말해 봐', '말해']:
             if s in text:
                 await channel.send("안녕!")
+                default = False
                 break
         for s in ['읊어봐', '읊어 봐', '읊어']:
             if s in text:
                 await channel.send("어흥 어흥 어흥")
+                default = False
                 break
         for s in ['글써봐', '글써 봐', '글 써봐', '글 써 봐']:
             if s in text:
                 await channel.send("나랏말싸미...")
+                default = False
                 break
 
-        # ##################################################
-        # commands for information
-        if '주식' in text and not is_help(text):
-            if '주요종목' in text:
-                stock = Stock(channel)
-                stock.all_major()
-            else:
-                stock = Stock(channel)
-            await stock.show()
+        ##################################################
+        #           commands for information             #
+        ##################################################
+        for s in ['주식']:
+            if s in text and not is_help(text):
+                if '주요종목' in text:
+                    stock = Stock(channel)
+                    stock.all_major()
+                else:
+                    stock = Stock(channel)
+                await stock.show()
+                default = False
 
-        if '코로나' in text and not is_help(text):
-            covid19 = Covid19(channel)
-            covid19.all()
-            await covid19.show()
+        for s in ['코로나']:
+            if s in text and not is_help(text):
+                covid19 = Covid19(channel)
+                covid19.all()
+                await covid19.show()
+                default = False
 
         for s in ['순위', '랭킹']:
             if s in text and not is_help(text):
-                if '프로그래밍' in text:
-                    url = "https://www.tiobe.com/tiobe-index/"
+                for keyword in ['프로그래밍', '플밍']:
+                    if keyword in text:
+                        ranking = Ranking(channel)
+                        ranking.programming()
+                        await ranking.show("")
+                        default = False
+                        break
+                for keyword in ['애니', '애니메이션', '아니메']:
+                    if keyword in text:
+                        ranking = Ranking(channel)
+                        ranking.anime()
+                        await ranking.show("")
+                        default = False
+                        break
+
+        ##################################################
+        #               commands for misc                #
+        ##################################################
+        for s in ['개발자', '만든 사람']:
+            if s in text and not is_help(text):
+                embed = discord.Embed(title='개발자 정보',
+                                      description="AI입니다. 하지만 할 수 있는 게 아무것도 없습니다.\n"
+                                                  "궁금하면 찾아가 보시죠?",
+                                      color=0x0f4c81)
+                embed.add_field(name='디미백과',
+                                value='https://dimipedia.net/wiki/%EC%82%AC%ED%98%B8%EC%A4%80',
+                                inline=False)
+                embed.add_field(name='깃허브',
+                                value='https://github.com/sqrtpi177',
+                                inline=False)
+                embed.add_field(name='블로그',
+                                value='https://blog.naver.com/moy0037',
+                                inline=False)
+                embed.add_field(name='페이스북',
+                                value='https://www.facebook.com/profile.php?id=100029757106182',
+                                inline=False)
+                embed.add_field(name="유튜브",
+                                value="https://www.youtube.com/channel/UCkxI0ozgMOV3Akpyr6QQ5RQ",
+                                inline=False)
+                await channel.send(embed=embed)
+
+        ##################################################
+        #   default: check for abbreviations and etc     #
+        ##################################################
+        if default and not is_help(text):
+            # 주식
+            for keyword in ['주식']:
+                if keyword in text:
+                    if '주요종목' in text:
+                        stock = Stock(channel)
+                        stock.all_major()
+                    else:
+                        stock = Stock(channel)
+                    await stock.show()
+
+            # 순위 - 프로그래밍
+            for keyword in ['프로그래밍', '플밍']:
+                if keyword in text:
                     ranking = Ranking(channel)
-                    ranking.programming(url)
+                    ranking.programming()
+                    await ranking.show("")
+                    break
+            # 순위 - 애니메이션
+            for keyword in ['애니', '애니메이션', '아니메']:
+                if keyword in text:
+                    ranking = Ranking(channel)
+                    ranking.anime()
                     await ranking.show("")
                     break
 
-        # ##################################################
-        # commands for misc
-        for s in ['도움', '도움말', '도와줘']:
-            if s in text:
-                if '인사' in text:
-                    embed = discord.Embed(title="인사",
-                                          description="호돌봇이 당신에게 인사합니다!\n"
-                                                      "[안녕]: 만남 인사\n"
-                                                      "[잘가]: 작별 인사",
-                                          color=0x04fc81)
-                elif '놀기' in text:
-                    embed = discord.Embed(title="놀기",
-                                          description="호돌봇이 당신과 놀아줍니다!\n"
-                                                      "재미 없을 지도 몰라요.\n"
-                                                      "[말해봐]: 말하기\n"
-                                                      "[읊어봐]: 시 읊기\n"
-                                                      "[글 써봐]: 글 쓰기",
-                                          color=0x04fc81)
-                elif '코로나' in text:
-                    embed = discord.Embed(title="코로나",
-                                          description="코로나19 현황을 알려줍니다.\n"
-                                                      "[코로나]: 코로나19 현황",
-                                          color=0x04fc81)
-                elif '주식' in text:
-                    embed = discord.Embed(title="주식",
-                                          description="주식 현황을 알려줍니다.\n"
-                                                      "아직은 코스피, 코스닥, 코스피200밖에 없어요.\n"
-                                                      "[주식]: 주식 데이터",
-                                          color=0x04fc81)
-                elif '순위' in text:
-                    embed = discord.Embed(title="순위",
-                                          description="각종 순위를 보여줍니다.\n"
-                                                      "별 신기한 게 다 있어요!\n"
-                                                      "[순위]-[프로그래밍]: 프로그래밍 언어 순위",
-                                          color=0x04fc81)
-                elif '개발자' in text:
-                    embed = discord.Embed(title="개발자",
-                                          description="저를 창조한 분이에요!\n"
-                                                      "[개발자]: '그'의 정보 확인하기",
-                                          color=0x04fc81)
-                else:
-                    embed = discord.Embed(title='봇 정보',
-                                          description="",
-                                          color=0x04fc81)
-                    embed.add_field(name='명령어 목록',
-                                    value="\n인사: [안녕], [잘가]"
-                                          "\n놀기: [말해봐], [읊어봐], [글 써봐]"
-                                          "\n주식: [주식]"
-                                          "\n코로나: [코로나]"
-                                          "\n순위: [순위]-[프로그래밍]"
-                                          "\n기타: [도움], [개발자]",
-                                    inline=False)
-                await channel.send(embed=embed)
-                break
-
-        if '개발자' in text and not is_help(text):
-            embed = discord.Embed(title='개발자 정보',
-                                  description="AI입니다. 하지만 할 수 있는 게 아무것도 없습니다.\n"
-                                              "궁금하면 찾아가 보시죠?",
-                                  color=0x0f4c81)
-            embed.add_field(name='디미백과',
-                            value='https://dimipedia.net/wiki/%EC%82%AC%ED%98%B8%EC%A4%80',
-                            inline=False)
-            embed.add_field(name='깃허브',
-                            value='https://github.com/sqrtpi177',
-                            inline=False)
-            embed.add_field(name='블로그',
-                            value='https://blog.naver.com/moy0037',
-                            inline=False)
-            embed.add_field(name='페이스북',
-                            value='https://www.facebook.com/profile.php?id=100029757106182',
-                            inline=False)
-            embed.add_field(name="유튜브",
-                            value="https://www.youtube.com/channel/UCkxI0ozgMOV3Akpyr6QQ5RQ",
-                            inline=False)
-            await channel.send(embed=embed)
+        ##################################################
+        #                       help                     #
+        ##################################################
+        if is_help(text):
+            for s in ['도움', '도움말', '도와줘']:
+                if s in text:
+                    if '인사' in text:
+                        embed = discord.Embed(title="인사",
+                                              description="호돌봇이 당신에게 인사합니다!\n"
+                                                          "[안녕]: 만남 인사\n"
+                                                          "[잘가]: 작별 인사",
+                                              color=0x04fc81)
+                    elif '놀기' in text:
+                        embed = discord.Embed(title="놀기",
+                                              description="호돌봇이 당신과 놀아줍니다!\n"
+                                                          "재미 없을 지도 몰라요.\n"
+                                                          "[말해봐]: 말하기\n"
+                                                          "[읊어봐]: 시 읊기\n"
+                                                          "[글 써봐]: 글 쓰기",
+                                              color=0x04fc81)
+                    elif '코로나' in text:
+                        embed = discord.Embed(title="코로나",
+                                              description="코로나19 현황을 알려줍니다.\n"
+                                                          "[코로나]: 코로나19 현황",
+                                              color=0x04fc81)
+                    elif '주식' in text:
+                        embed = discord.Embed(title="주식",
+                                              description="주식 현황을 알려줍니다.\n"
+                                                          "아직은 코스피, 코스닥, 코스피200밖에 없어요.\n"
+                                                          "[주식]: 주식 데이터",
+                                              color=0x04fc81)
+                    elif '순위' in text:
+                        embed = discord.Embed(title="순위",
+                                              description="각종 순위를 보여줍니다.\n"
+                                                          "별 신기한 게 다 있어요!\n"
+                                                          "[순위]\n"
+                                                          "-[프로그래밍]: 프로그래밍 언어 순위\n"
+                                                          "-[애니]: ㄴㄷㅆ",
+                                              color=0x04fc81)
+                    elif '개발자' in text:
+                        embed = discord.Embed(title="개발자",
+                                              description="저를 창조한 분이에요!\n"
+                                                          "[개발자]: '그'의 정보 확인하기",
+                                              color=0x04fc81)
+                    else:
+                        embed = discord.Embed(title='봇 정보',
+                                              description="",
+                                              color=0x04fc81)
+                        embed.add_field(name='명령어 목록',
+                                        value="\n인사: [안녕], [잘가]"
+                                              "\n놀기: [말해봐], [읊어봐], [글 써봐]"
+                                              "\n주식: [주식]"
+                                              "\n코로나: [코로나]"
+                                              "\n순위: [순위]-[프로그래밍], [애니]"
+                                              "\n기타: [도움], [개발자]",
+                                        inline=False)
+                    await channel.send(embed=embed)
+                    break
 
 
 # check if it's help command
--- ranking.py
+++ ranking.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import requests
 from urllib.request import urlopen
 import discord
 
@@ -9,7 +10,8 @@ class Ranking:
         self.message = ""
         self.embed = discord.Embed()
 
-    def programming(self, url):
+    def programming(self):
+        url = "https://www.tiobe.com/tiobe-index/"
         html = urlopen(url)
         soup = BeautifulSoup(html, "html.parser")
         table = soup.find("table", {"id": "top20"})
@@ -22,7 +24,7 @@ class Ranking:
         for i, data in enumerate(lang_list):
             self.message += "{}위: {}\n".format(i + 1, data)
 
-        # send message
+        # set message
         self.embed = discord.Embed(title='프로그래밍 언어 순위',
                                    color=0x0f4c81)
         self.embed.add_field(name="2020년 프로그래밍 언어 순위입니다.",
@@ -31,6 +33,34 @@ class Ranking:
                                    + '---------------------------------------------',
                              inline=False)
         self.embed.set_footer(text='정보 제공: TIOBE')
+
+    def anime(self):
+        url = "http://anime.onnada.com/rank.php"
+        html = requests.get(url)
+
+        soup = BeautifulSoup(html.content.decode('utf-8', 'replace'), 'html.parser')
+        tables = soup.find_all('table', class_='web-array')
+        tr_list = tables[0].find_all('tr')
+        # range(4, 23, 2)
+        td_list = []
+        for i in range(3, 23, 2):
+            td_list.append(tr_list[i])
+        anime_list = []
+        for item in td_list:
+            anime_list.append(item.find_all('td')[2].find('a').text)
+
+        for i, data in enumerate(anime_list):
+            self.message += "{}위: {}\n".format(i + 1, data)
+
+        # set message
+        self.embed = discord.Embed(title='애니메이션 순위',
+                                   color=0x0f4c81)
+        self.embed.add_field(name="애니메이션 순위입니다.",
+                             value='---------------------------------------------\n'
+                                   + self.message
+                                   + '---------------------------------------------',
+                             inline=False)
+        self.embed.set_footer(text='정보 제공: 온나다')
 
     async def show(self, message):
         await self.channel.send(message, embed=self.embed)
@@ -41,5 +71,9 @@ class Ranking:
 
 if __name__ == '__main__':
     ranking = Ranking(None)
-    ranking.programming("https://www.tiobe.com/tiobe-index")
+    ranking.programming()
+    ranking.debug()
+
+    ranking = Ranking(None)
+    ranking.anime()
     ranking.debug()
