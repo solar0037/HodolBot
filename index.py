8858987933979cc2dfd3717df24d4063ec3dc3a0
sqrtpi177 <solar0037@gmail.com>
2020-10-03 14:17:50 +0900

    refactor: use discord.ext.commands.Bot

--- index.py
+++ index.py
@@ -3,24 +3,24 @@
 깃허브 레포지토리: https://github.com/sqrtpi177/hodol-bot
 """
 
-import discord
+from discord.ext.commands import Bot
 from app import on_ready_fn, on_message_fn
 
 
-class BotClient(discord.Client):
+class HodolBot(Bot):
     def __init__(self):
-        super().__init__()
+        super().__init__(command_prefix='!')
 
     async def on_ready(self):
-        await on_ready_fn(client)
+        await on_ready_fn(self)
 
     async def on_message(self, message):
-        await on_message_fn(message)
+        await on_message_fn(client, message)
 
 
 if __name__ == '__main__':
     with open('./token.txt', 'r', encoding='utf-8') as f:
         token = f.readline()  # token 유출 방지
 
-    client = BotClient()
+    client = HodolBot()
     client.run(token)
