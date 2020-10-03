930a87b67b8e470edd7e0465a26e9d2199127aa0
sqrtpi177 <solar0037@gmail.com>
2020-10-03 10:19:58 +0900

    refactor: client modularization

--- index.py
+++ index.py
@@ -6,22 +6,21 @@
 import discord
 from app import on_ready_fn, on_message_fn
 
-client = discord.Client()
 
+class BotClient(discord.Client):
+    def __init__(self):
+        super().__init__()
 
-# 로그인
-@client.event
-async def on_ready():
-    await on_ready_fn(client)
+    async def on_ready(self):
+        await on_ready_fn(client)
 
-
-# 메세지 처리
-@client.event
-async def on_message(message):
-    await on_message_fn(message)
+    async def on_message(self, message):
+        await on_message_fn(message)
 
 
 if __name__ == '__main__':
     with open('./token.txt', 'r', encoding='utf-8') as f:
         token = f.readline()  # token 유출 방지
+
+    client = BotClient()
     client.run(token)
