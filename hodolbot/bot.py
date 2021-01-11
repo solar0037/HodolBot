c76a379140cf422fe4786d6a9eca5d03d6222a69
solar0037 <solar0037@gmail.com>
2021-01-11 11:22:37 +0900

    update runtime and use PRODUCTION env variable

--- hodolbot/bot.py
+++ hodolbot/bot.py
@@ -2,12 +2,17 @@ from discord import TextChannel, Member
 from discord.ext.commands import Bot
 
 from hodolbot.functions import greetings_fn, views_fn, info_fn
-from hodolbot.constants import BOT_CALL, LINE
+from hodolbot.utils.get_env import get_env
+from hodolbot.constants import LINE, DEVELOPMENT_BOT_CALL, PRODUCTION_BOT_CALL
 
 
 class HodolBot(Bot):
     def __init__(self):
-        super().__init__(command_prefix=BOT_CALL)
+        if int(get_env("PRODUCTION", 0)):
+            self.BOT_CALL = PRODUCTION_BOT_CALL
+        else:
+            self.BOT_CALL = DEVELOPMENT_BOT_CALL
+        super().__init__(command_prefix=self.BOT_CALL)
 
     async def on_ready(self) -> None:
         print(f"{LINE}\n" \
@@ -21,9 +26,9 @@ class HodolBot(Bot):
         content: str = message.content
         channel: TextChannel = message.channel
 
-        if content.startswith(BOT_CALL):
+        if content.startswith(self.BOT_CALL):
             # ![명령어] -> [명령어]
-            text = content.replace(BOT_CALL, "").strip()
+            text = content.replace(self.BOT_CALL, "").strip()
             print(f"message: {text}")
 
             await greetings_fn(channel, author, text)
--- hodolbot/constants/__init__.py
+++ hodolbot/constants/__init__.py
@@ -1,3 +1,5 @@
 BOT_NAME = "호돌봇"  # 봇 이름
-BOT_CALL = "!"  # 봇 호출
+# BOT_CALL = "!"  # 봇 호출
+DEVELOPMENT_BOT_CALL = "."
+PRODUCTION_BOT_CALL = "!"
 LINE = "-"*15
